d995472b7f77298fd5b0d649f157cfb73be258d0
AbdulHameed <anofiolawale@gmail.com>
2023-04-30 02:57:27 +0100

    Implementation/ created model and methods

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Image;
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    public function addImage(Request $request)
+    {
+        $request->validate([
+            'label' => 'required',
+            'image_url' => 'required'
+        ]);
+
+        $uploader = new Image;
+        $uploader->image_url = $request->image_url;
+        $uploader->label = $request->label;
+        $uploader->save();
+
+        return response(201)->json("Image saved successfuly");
+
+    }
+
+    public function getImages()
+    {
+        return Image::pluck('image_url', 'label')->orderBy('desc');
+    }
+
+    public function searchByLabel(string $label)
+    {
+        $result = Image::where('label', $label);
+        return $result;
+    }
+
+    public function deleteImage()
+    {
+
+    }
+}
