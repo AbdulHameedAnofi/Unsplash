6e0bf052dcc8b4e5bf54fe4fc4e7cc2aaf07fc0b
AbdulHameed <anofiolawale@gmail.com>
2023-05-03 23:50:09 +0100

    addition/ added the delete method

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        Artisan::call('migrate:fresh');
 
         \App\Models\User::factory()->create([
             'name' => 'John Doe',
